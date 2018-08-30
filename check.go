61ebc15290797f4567b68b6eac8b90375331b7fe
Alexander A. Klimov <grandmaster@al2klimov.de>
2018-08-30 07:07:18 +0200

    Execute a check and print the result

---
+++ check.go
@@ -0,0 +1,32 @@
+package go_monplug_utils
+
+import (
+	"fmt"
+	"golang.org/x/crypto/ssh/terminal"
+	"os"
+)
+
+func ExecuteCheck(
+	onTerminal func() (output string),
+	check func() (status uint8, output string, perfdata PerfdataCollection, errors map[string]error),
+) (exit int) {
+	if terminal.IsTerminal(int(os.Stdout.Fd())) {
+		fmt.Print(onTerminal())
+		return 3
+	}
+
+	status, output, perfdata, errsChk := check()
+	if errsChk != nil {
+		for context, err := range errsChk {
+			fmt.Printf("%s: %s\n", context, err.Error())
+		}
+
+		return 3
+	}
+
+	if _, errFP := fmt.Print(output + perfdata.String()); errFP != nil {
+		return 3
+	}
+
+	return int(status)
+}
